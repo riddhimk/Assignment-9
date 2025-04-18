0bb4491449b004fb53647ef541cb6aee2855dff2
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-18 22:47:48 +0530

    Created DBConnection,java

---
+++ DBConnection.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class DBConnection
+{
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "riddhim10000";
+
+    public static Connection getConnection()
+            throws SQLException
+    {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
